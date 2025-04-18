f5c3f71208ec2cb8c125b3c62acc467fdb08779c
unxtcopilotuser14 <copilotjan24user14@nuvelabs.com>
2025-04-18 05:15:26 +0000

    Initial commit

---
+++ src/main/java/com/ey/in/TxnpreprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.ey.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnpreprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxnpreprocessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ey/in/TxnpreprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ey.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxnpreprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
